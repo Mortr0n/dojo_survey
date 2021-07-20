4d77dbd72f7ac572e41c463ce5b5235e2a3b5cfd
Chris <cmsl4y3r@gmail.com>
2021-07-19 19:16:40 -0600

    dojo survey revisited complete

--- dojo_survey_app/urls.py
+++ dojo_survey_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('session_items', views.session_items),
     path('results', views.results),
 ]
--- dojo_survey_app/views.py
+++ dojo_survey_app/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render, redirect, HttpResponse
 
+
 def index(request):
     return render(request, "index.html")
 
-def results(request):
-    context = {
-        'name' : request.POST["name"],
-        'location'	: request.POST['location'],
-        'language'	: request.POST['language'],
-        'comments'  : request.POST['comments'],
+# def results(request):
+# Keeping this for posterity
+#     # request.session['name'] = request.POST['name']
+#     # request.session['location'] = request.POST['location']
+#     # request.session['language'] = request.POST['language']
+#     # request.session['comments'] = request.POST['comments']
+#     # if request.method=="POST":
+#     #     name = request.POST["name"]
+#     #     location = request.POST["location"]
+#     #     language = request.POST["language"]
+#     #     comments = request.POST["comments"]
+#     return render(request, "/results.html")
+
+
+def session_items(request):
+    request.session['results'] = {
+        'name': request.POST['name'],
+        'location': request.POST['location'],
+        'language': request.POST['language'],
+        'comments': request.POST['comments'],
     }
-    if request.method=="POST":
-        name = request.POST["name"]
-        location = request.POST["location"]
-        language = request.POST["language"]
-        comments = request.POST["comments"]
-    return render(request, "results.html", context)
+    return redirect('/results')
+
+
+def results(request):
+    context = {  # context could be anything as long as we call it below
+        'results': request.session['results']
+    }
+    return render(request, "results.html", context)  # see context here!
